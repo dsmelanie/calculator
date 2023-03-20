1ee84987dc55ba3a60d90004560efd4b16bd6f63
Mélanie <dsmelanie@outlook.fr>
2023-03-20 11:17:34 +0100

    Event listener on keyboard keys

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,53 +1,66 @@
 class Calculator {
-    constructor() {
-      this.calculator = document.createElement("div");
-      this.calculator.classList.add("calculator");
-      document.body.appendChild(this.calculator);
-  
-      this.display = document.createElement("input");
-      this.display.setAttribute("type", "text");
-      this.display.setAttribute("readonly", true);
-      this.display.classList.add("display");
-      this.calculator.appendChild(this.display);
-  
-      const buttons = [
-        ["7", "8", "9","+"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "*"],
-        ["0", ".", "C", "/"],
-        ["="]
-      ];
-  
-      buttons.forEach((row) => {
-        const buttonRow = document.createElement("div");
-        buttonRow.classList.add("row");
-        this.calculator.appendChild(buttonRow);
-  
-        row.forEach((buttonLabel) => {
-          const button = document.createElement("button");
-          button.innerHTML = buttonLabel;
-          buttonRow.appendChild(button);
+  constructor() {
+    this.calculator = document.createElement("div");
+    this.calculator.classList.add("calculator");
+    document.body.appendChild(this.calculator);
 
-          if (["+", "-", "*", "/"].includes(buttonLabel)) {
-            button.classList.add("btn-operator");
+    this.display = document.createElement("input");
+    this.display.setAttribute("type", "text");
+    this.display.setAttribute("readonly", true);
+    this.display.classList.add("display");
+    this.calculator.appendChild(this.display);
+
+    const buttons = [
+      ["7", "8", "9", "+"],
+      ["4", "5", "6", "-"],
+      ["1", "2", "3", "*"],
+      ["0", ".", "C", "/"],
+      ["="],
+    ];
+
+    buttons.forEach((row) => {
+      const buttonRow = document.createElement("div");
+      buttonRow.classList.add("row");
+      this.calculator.appendChild(buttonRow);
+
+      row.forEach((buttonLabel) => {
+        const button = document.createElement("button");
+        button.innerHTML = buttonLabel;
+        buttonRow.appendChild(button);
+
+        if (["+", "-", "*", "/"].includes(buttonLabel)) {
+          button.classList.add("btn-operator");
+        } else if (buttonLabel === "=") {
+          button.classList.add("btn-equal");
+        }
+
+        button.addEventListener("click", () => {
+          if (buttonLabel === "C") {
+            this.display.value = "";
           } else if (buttonLabel === "=") {
-            button.classList.add("btn-equal");
+            const result = eval(this.display.value);
+            this.display.value = result.toString();
+          } else {
+            this.display.value += buttonLabel;
           }
-  
-          button.addEventListener("click", () => {
-            if (buttonLabel === "C") {
-              this.display.value = "";
-            } else if (buttonLabel === "=") {
-              const result = eval(this.display.value);
-              this.display.value = result.toString();
-            } else {
-              this.display.value += buttonLabel;
-            }
-          });
         });
       });
-    }
+    });
+
+    window.addEventListener("keydown", (event) => {
+      const key = event.key;
+      if (/[.0-9*+/-]/.test(key)) {
+        this.display.value += key;
+      } else if (key === "Enter") {
+        const result = eval(this.display.value);
+        this.display.value = result.toString();
+      } else if (key === "Delete") {
+        this.display.value = "";
+      } else if (key === "Backspace") {
+        this.display.value = this.display.value.slice(0, -1);
+      }
+    });
   }
-  
-  new Calculator();
-  
+}
+
+new Calculator();
